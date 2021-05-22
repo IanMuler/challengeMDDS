8c0a3c7f34759cba1647c969e7705b1d53e45c86
Ian Muler <Yoelmuler@gmail.com>
2021-05-22 14:39:26 -0300

    Exercise R4

--- code/js/R4.js
+++ code/js/R4.js
@@ -8,17 +8,20 @@ fetch(API)
 .then(data => printHTML(data))
 
 function printHTML(data){
-    const APIdata = data.data
-    
+    const APIdata = data.data.filter(news => (news.image)? true : false)
+
     let count = 1;
 
     APIdata.map( data => { //crear un elemento por cada dato
+
+    const shortDescription = data.description.split(' ').splice(0, 20).join(' ') //recorta descripción a n palabras
+
     const news = document.createElement('ul'); 
     news.setAttribute("id", `${count++}`);
     news.innerHTML = `
-    <li class="li_img"><img src="${data.image}"></li>
+    <li class="li_img"><img src="${data.image}" alt="News image"></li>
     <li class="li_title">${data.title}</li>
-    <li class="li_description">${data.description}</li>
+    <li class="li_description">${shortDescription}[...]</li>
     <li>${data.author}</li>
     <li>${data.published_at.split("T")[0]}</li> 
     <li><button><a href="${data.url}">Visit</a></button></li>    
@@ -27,17 +30,21 @@ function printHTML(data){
 })
 }
 
-// buttons functions
+// buttons functions (vista TABLET-MOBILE)
 
 const btnPrevious = document.getElementById('btn_previous')
 const btnNext = document.getElementById('btn_next')
 
+window.addEventListener('resize', () => { // listener para acomodar el scroll cuando pasa a vista TABLET o menor
+    if(body.offsetWidth <= 800) window.scroll(0,0) 
+    }) 
+
 btnPrevious.addEventListener('click', () => {
-    listHeight = body.offsetHeight;
+    listHeight = body.offsetHeight;  // height de cada lista
     if(window.scrollY > 0){
-        window.scrollY -= listHeight
+        window.scrollY -= listHeight // valor para hacer scroll una lista atrás
     } else {
-        window.scrollY = body.scrollHeight - listHeight;
+        window.scrollY = body.scrollHeight - listHeight; // valor para hacer scroll hasta la última lista
     }
     window.scroll(0, window.scrollY)
 })
